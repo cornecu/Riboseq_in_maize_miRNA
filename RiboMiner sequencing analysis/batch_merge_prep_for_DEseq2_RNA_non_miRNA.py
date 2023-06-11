1348f096e709b4c87eb64e873f8895aa0a794e77
cornecu <72764945+cornecu@users.noreply.github.com>
2023-06-10 22:33:01 -0400

    step3 prepare files for DESeq2 analysis

---
+++ RiboMiner sequencing analysis/batch_merge_prep_for_DEseq2_RNA_non_miRNA.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+'''
+Hailong create this scripts to batch prepare the file for DESeq2 analysis.
+
+'''
+
+import sys, os
+import numpy as np
+import pandas as pd
+import glob
+''' The glob module in python is used to retrieve files or pathnames matching a specified pattern'''
+##### import the file
+# importing packages
+  
+folder_path = '/opt/home/thompsonlab/Riboseq_HY/ribominer/ribosome_distribution/RNA/non_miRNA'
+file_list = glob.glob(folder_path + "/*.txt")
+## index_col will tell us which column as the index column
+main_dataframe = pd.DataFrame(pd.read_table(file_list[0], index_col = 'transcript_id'))
+  
+for i in range(1,len(file_list)):
+    data = pd.read_table(file_list[i], index_col = 'transcript_id')
+    df = pd.DataFrame(data)
+    ## axis =1 will tell us to merge based on the index names
+    main_dataframe = pd.concat([main_dataframe, df], axis = 1)
+  
+# print(main_dataframe)
+  
+# creating a new csv file with
+# the dataframe we created
+# main_dataframe.to_csv('new_csv1.csv')
+
+for c in main_dataframe.columns:
+    main_dataframe[c].to_csv(c + '.txt', index = True, sep = '\t', header = False)
